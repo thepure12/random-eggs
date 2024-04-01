891074b482fd87a819b5fa48cf20b587cf2eb076
thepure12 <renninger12@gmail.com>
2024-04-01 11:39:08 -0400

    Update random_eggs.js
    Added go brr button.

--- random_eggs.js
+++ random_eggs.js
@@ -1,29 +1,41 @@
-function addEasterEgg() {
-    const easterEggs = ["🐰", "🥚", "🌷", "🐣", "🍫", "✝️", "🩸", "🪨"];
+function addEasterEgg(amount = 1) {
+  const easterEggs = ["🐰", "🥚", "🌷", "🐣", "🍫", "✝️", "🩸", "🪨"];
+  // console.log(randomEgg)
+
+  for (let i = 0; i < amount; i++) {
     const randomEgg = easterEggs[Math.floor(Math.random() * easterEggs.length)];
-    // console.log(randomEgg)
-    
-    const eggElement = document.createElement('span');
-    eggElement.classList.add('easter-egg');
+    const eggElement = document.createElement("span");
+    eggElement.classList.add("easter-egg");
     eggElement.innerHTML = randomEgg;
 
-    const maxX = document.body.clientWidth - 50;
-    const maxY = document.body.clientHeight - 50;
+    const maxX = document.body.clientWidth - 5;
+    const maxY = document.body.clientHeight - 5;
 
     const randomX = Math.floor(Math.random() * maxX);
     const randomY = Math.floor(Math.random() * maxY);
 
-    eggElement.style.left = randomX + 'px';
-    eggElement.style.top = randomY + 'px';
+    eggElement.style.left = randomX + "px";
+    eggElement.style.top = randomY + "px";
 
-    eggElement.addEventListener('click', () => {
+    eggElement.addEventListener("click", () => {
       eggElement.remove();
     });
 
     document.body.appendChild(eggElement);
   }
+}
 
-function setEggInterval(interval) {
- // Add Easter eggs every 3 seconds
-  setInterval(addEasterEgg, interval);
+function setEggInterval(interval=3000, amountPerInterval=1) {
+  // Add Easter eggs every interval milliseconds
+  setInterval(addEasterEgg, interval, amountPerInterval);
+}
+
+function addGoBrrButton() {
+  const brrElement = document.createElement("button");
+  brrElement.innerText = "Go Brr!"
+  brrElement.classList.add("go-brr");
+  brrElement.addEventListener("click",()=>{
+    setEggInterval(1, 10)
+  })
+  document.body.appendChild(brrElement);
 }
